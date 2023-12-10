6b0d5aa3a4d18c0d81e245c88414af9dfe3fbaa3
ben <ygnben21@gmail.com>
2023-12-10 22:12:33 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 // import {} from "";
 import { Card } from "@mui/material";
+
 function App() {
   const [book, setBook] = useState(null);
   function getBook() {}
@@ -14,25 +15,26 @@ function App() {
 function FilterableBookTable({}) {
   return;
 }
-function bookInfo(){
-  return <>
-  <div>
-    bookInfo
-  </div>
-  </>
+function bookInfo() {
+  return (
+    <>
+      <div>bookInfo</div>
+    </>
+  );
 }
 function SearchBar() {
-  return <>
-  <form>
-  <input type="
-    text" value={filterText} placeholder="Search...">
-    </input>
-
-  </form>
- 
-   
- 
-  </>
+  return (
+    <>
+      <form>
+        <input
+          type="
+    text"
+          value={filterText}
+          placeholder="Search..."
+        ></input>
+      </form>
+    </>
+  );
 }
 
 function BookTable() {}
