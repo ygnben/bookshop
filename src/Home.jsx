27e9d5c4f7e001a4e30a8db279cf91b3993f5b89
ygnben <ygnben21@gmail.com>
2023-12-11 11:31:00 +0800

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,23 +12,125 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import {
+  Card,
+  Button,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+
+import SwipeableTextMobileStepper from "./SwipeableTextMobileStepper";
+import PrimarySearchAppBar from "./PrimarySearchAppBar";
 
 function Book({ title }) {
   return (
-    <div>
-      <img src="" alt="" />
-      <div>title:{title}</div>
-      <div>Author:</div>
-      <div>price</div>
-      <div>Detail:</div>
-      <button>faourite</button>
-      <button>Buy</button>
-    </div>
+    // <div>
+    //   <img src="" alt="" />
+    //   <div>title:{title}</div>
+    //   <div>Author:</div>
+    //   <div>price</div>
+    //   <div>Detail:</div>
+    //   <button>faourite</button>
+    //   <button>Buy</button>
+    // </div>
+
+    // <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    //   <Box sx={{ my: 3, mx: 2 }}>
+    //     <Grid container alignItems="center">
+    //       <Grid item xs>
+    //         <Typography gutterBottom variant="h4" component="div">
+    //           Toothbrush
+    //         </Typography>
+    //       </Grid>
+    //       <Grid item>
+    //         <Typography gutterBottom variant="h6" component="div">
+    //           $4.50
+    //         </Typography>
+    //       </Grid>
+    //     </Grid>
+    //     <Typography color="text.secondary" variant="body2">
+    //       Pinstriped cornflower blue cotton blouse takes you on a walk to the
+    //       park or just down the hall.
+    //     </Typography>
+    //   </Box>
+    //   <Divider variant="middle" />
+    //   <Box sx={{ m: 2 }}>
+    //     <Typography gutterBottom variant="body1">
+    //       Select type
+    //     </Typography>
+    //     <Stack direction="row" spacing={1}>
+    //       <Chip label="Extra Soft" />
+    //       <Chip color="primary" label="Soft" />
+    //       <Chip label="Medium" />
+    //       <Chip label="Hard" />
+    //     </Stack>
+    //   </Box>
+    //   <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
+    //     <Button>Add to cart</Button>
+    //   </Box>
+    // </Box>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }
 
 function BookList() {
-  return <Book title="a"></Book>;
+  return (
+    <div style={{ width: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          p: 1,
+          m: 1,
+          bgcolor: "background.paper",
+          // maxWidth: 300,
+          borderRadius: 1,
+        }}
+      >
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+        <Book title="a"></Book>
+      </Box>
+    </div>
+  );
+
+  // <Book title="a"></Book>;
 }
 
 function SearchBar() {
@@ -113,21 +215,46 @@ function SearchBar() {
 }
 
 function PictureBar() {
-  return <div>picture bar</div>;
+  return <SwipeableTextMobileStepper />;
 }
 
 function CategoryBar() {
-  return <div>CategoryBar</div>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: "background.paper" }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs>
+    </Box>
+  );
 }
 
 function Home() {
   return (
-    <>
-      <SearchBar />
+    <div style={{ width: "100%" }}>
+      {/* <SearchBar /> */}
+      <PrimarySearchAppBar />
       <PictureBar />
       <CategoryBar />
       <BookList />
-    </>
+    </div>
   );
 }
 
