4ce02037d4e34518a41e152cea395767111db0f8
ben <ygnben21@gmail.com>
2023-12-19 20:35:08 +0800

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,55 @@
-import { useState } from "react";
-// import "./App.css";
-// import {} from "";
-import { Card } from "@mui/material";
+// import { useState } from "react";
+// // import "./App.css";
+// // import {} from "";
+// import { Card } from "@mui/material";
+
+// function App() {
+//   const [book, setBook] = useState(null);
+//   function getBook() {}
+//   return (
+//     <>
+//       <Card>Hello</Card>
+//     </>
+//   );
+// }
+// function FilterableBookTable({}) {
+//   return;
+// }
+// function bookInfo() {
+//   return (
+//     <>
+//       <div>bookInfo</div>
+//     </>
+//   );
+// }
+// function SearchBar() {
+//   return (
+//     <>
+//       <form>
+//         <input
+//           type="
+//     text"
+//           value={filterText}
+//           placeholder="Search..."
+//         ></input>
+//       </form>
+//     </>
+//   );
+// }
+
+// function BookTable() {}
+
+// export default App;
+
+import { AppProvider } from "@/providers/app";
+import { AppRoutes } from "@/routes";
 
 function App() {
-  const [book, setBook] = useState(null);
-  function getBook() {}
   return (
-    <>
-      <Card>Hello</Card>
-    </>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
   );
 }
-function FilterableBookTable({}) {
-  return;
-}
-function bookInfo() {
-  return (
-    <>
-      <div>bookInfo</div>
-    </>
-  );
-}
-function SearchBar() {
-  return (
-    <>
-      <form>
-        <input
-          type="
-    text"
-          value={filterText}
-          placeholder="Search..."
-        ></input>
-      </form>
-    </>
-  );
-}
-
-function BookTable() {}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,58 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+
+// import App from "./App.jsx";
+// import Home from "./Home.jsx";
+// import Detail from "./Detail.jsx";
+// import SignIn from "./features/SignIn.jsx";
+// import Search from "./Search.jsx";
+// import Favourite from "./Favourite.jsx";
+
+// import store from "./redux/store.jsx";
+// // import "./index.css";
+
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   gql,
+// } from "@apollo/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// const client = new ApolloClient({
+//   // uri: "https://www.googleapis.com/books/v1/graphql",
+//   uri: "https://www.googleapis.com/books/v1/volumes",
+//   cache: new InMemoryCache(),
+// });
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <ApolloProvider client={client}>
+//         <BrowserRouter>
+//           <Routes>
+//             <Route exact path="/" element={<Home />} />
+//             <Route path="/Detail/:id" element={<Detail />} />
+//             <Route path="/Login" element={<SignIn />} />
+//             <Route path="/Home" element={<Home />} />{" "}
+//             <Route path="/Search" element={<Search />} />
+//             <Route path="/Favourite" element={<Favourite />} />
+//           </Routes>
+//         </BrowserRouter>
+//       </ApolloProvider>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 
 import App from "./App.jsx";
-import Home from "./Home.jsx";
-import Detail from "./Detail.jsx";
-import SignIn from "./features/SignIn.jsx";
-import Search from "./Search.jsx";
-import Favourite from "./Favourite.jsx";
-
-import store from "./redux/store.jsx";
-// import "./index.css";
-
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const client = new ApolloClient({
-  // uri: "https://www.googleapis.com/books/v1/graphql",
-  uri: "https://www.googleapis.com/books/v1/volumes",
-  cache: new InMemoryCache(),
-});
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/Detail/:id" element={<Detail />} />
-            <Route path="/Login" element={<SignIn />} />
-            <Route path="/Home" element={<Home />} />{" "}
-            <Route path="/Search" element={<Search />} />
-            <Route path="/Favourite" element={<Favourite />} />
-          </Routes>
-        </BrowserRouter>
-      </ApolloProvider>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
