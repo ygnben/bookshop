e0d1a75659d08eb9096563b2c3a5196e8c9a6368
ygnben <ygnben21@gmail.com>
2023-12-20 15:18:35 +0800

    updated check out

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,10 +1,17 @@
 import * as React from "react";
+
+import Swal from "sweetalert2";
+
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+
+import Typography from "@mui/material/Typography";
+
+import { useNavigate } from "react-router-dom";
 
 export default function Checkout({ state, setState, total, books }) {
   //   const [open, setOpen] = React.useState(false);
@@ -13,8 +20,19 @@ export default function Checkout({ state, setState, total, books }) {
   //     setOpen(true);
   //   };
 
+  const navigate = useNavigate();
+  console.log("books1111", books);
   const handleClose = () => {
     setState(false);
+
+    Swal.fire({
+      title: "Success",
+      text: "Thank you",
+      icon: "success",
+    });
+
+    localStorage.removeItem("shop");
+    navigate("/Home");
   };
   return (
     <>
@@ -27,25 +45,24 @@ export default function Checkout({ state, setState, total, books }) {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Check out"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">{"Check out book"}</DialogTitle>
         <DialogContent>
           {/* <DialogContentText id="alert-dialog-description">
             Let Google help apps determine location. This means sending
             anonymous location data to Google, even when no apps are running.
           </DialogContentText> */}
-          book:
-          {/* {books?.items.map((book) => (
-            <Book
-              key={book.id}
-              id={book.id}
-              title={book.volumeInfo.title}
-              img={book.volumeInfo.imageLinks}
-              desc={book.volumeInfo.description}
-              price={book.saleInfo?.listPrice?.amount}
-              curCode={book.saleInfo?.listPrice?.currencyCode}
-              login={login}
-            ></Book> */}
-          ))} total:{total}
+          {books.map((book) =>
+            book.map((book) => (
+              <>
+                <Typography>{book.volumeInfo.title} </Typography>
+                <Typography>
+                  price:{book.saleInfo?.listPrice?.amount || 50}
+                </Typography>
+              </>
+            ))
+          )}
+          <Typography>Total book:{books.length}</Typography>
+          <Typography> Total Price:{total}</Typography>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
